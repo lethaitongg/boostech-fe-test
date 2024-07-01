20e88bc331812b766bf5dc2c774515038902aec2
Long Le <lethaitongg@gmail.com>
2024-07-01 17:21:59 +0700

    align start menu

--- src/app/common/components/button-icon/button-icon.component.ts
+++ src/app/common/components/button-icon/button-icon.component.ts
@@ -13,14 +13,15 @@ import { MatRippleModule } from "@angular/material/core";
 export class ButtonIconComponent {
 	@Input() icon: string = "setting";
 	@Input()
-	color: string = `
-		linear-gradient(
-		0deg, 
-		rgba(255, 255, 255, 0.07) 0%, 
-		rgba(255, 255, 255, 0.07) 100%), 
-		linear-gradient(0deg, rgba(94.35, 94.35, 94.35, 0.18) 0%, 
-		rgba(94.35, 94.35, 94.35, 0.18) 100%)
-	`;
+	color: string = "";
+	// `
+	// 	linear-gradient(
+	// 	0deg,
+	// 	rgba(255, 255, 255, 0.07) 0%,
+	// 	rgba(255, 255, 255, 0.07) 100%),
+	// 	linear-gradient(0deg, rgba(94.35, 94.35, 94.35, 0.18) 0%,
+	// 	rgba(94.35, 94.35, 94.35, 0.18) 100%)
+	// `
 	@Input() size: string = "45px";
 	@Input() rounded: string = "87.782px";
 	style: Record<string, string | undefined | null> = {};
